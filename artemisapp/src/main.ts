063032afbbbc070cbf7ae79198a6c5bb0f787025
Leandro Zamudio León <lzamudio@soporta.cl>
2024-05-19 22:49:38 -0400

    Nueva capa + highlight + constraint

--- artemisapp/src/main.ts
+++ artemisapp/src/main.ts
@@ -1,85 +1,101 @@
 import './style.css';
 import ArcGISMap from '@arcgis/core/Map.js';
 import MapView from '@arcgis/core/views/MapView.js';
-import { watch } from '@arcgis/core/core/reactiveUtils.js';
 //Only if you are using an API key
 //import esriConfig from '@arcgis/core/config.js';
 import FeatureLayer from '@arcgis/core/layers/FeatureLayer.js';
+import Color from "@arcgis/core/Color.js";
+import SimpleFillSymbol from "@arcgis/core/symbols/SimpleFillSymbol.js";
+import SimpleLineSymbol from "@arcgis/core/symbols/SimpleLineSymbol.js";
+import SimpleRenderer from "@arcgis/core/renderers/SimpleRenderer.js";
+import GraphicsLayer from "@arcgis/core/layers/GraphicsLayer.js";
+
 
 const app = document.querySelector<HTMLDivElement>('#app')!
+
+const countryGraphicsLayer = new GraphicsLayer({
+  effect: "bloom(1.1, 0.2px, 0.2)",
+});
 
 //Only if you are using an API key
 //esriConfig.apiKey = 'MyAPIKey'
 
-// Create a map and add a feature layer
-const layer = new FeatureLayer({
-  url: 'https://services1.arcgis.com/LsoiDXzijohT7g97/arcgis/rest/services/Chile_ZonasNaturales/FeatureServer',
-})
-const map = new ArcGISMap({
-  basemap: 'streets-vector',
-  layers: [layer],
+let zz = new SimpleFillSymbol({
+  color: [227, 139, 79, 0.8],
+  outline: {
+      "color": [110, 110, 110],
+      "width": 1
+  }
 })
 
-// Set the initial rotation of the map view
-let rotation: number;
-if (window.innerWidth <= 480) {
-  rotation = 0;
-} else {
-  rotation = -90;
-}
+const simpleFillSymbol = new SimpleFillSymbol({
+  color: new Color([68,162,113,1]),
+  outline: new SimpleLineSymbol({
+    cap: "square",
+    color: new Color([255,255,255,1]),
+    join: "round",
+    miterLimit: 1,
+    style: "solid",
+    width: 1
+  }),
+  style: "solid"
+});
+
+const datarend = new SimpleRenderer({
+  symbol: simpleFillSymbol
+})
+
+// Create a map and add a feature layer
+const layer = new FeatureLayer({
+  url: 'https://services1.arcgis.com/LsoiDXzijohT7g97/arcgis/rest/services/WorldData/FeatureServer',
+  renderer: datarend
+})
+
+
+const map = new ArcGISMap({
+  layers: [layer,countryGraphicsLayer],
+})
+
 
 // Create a map view
 const view = new MapView({
   map,
   container: app,
-  zoom: 4,
-  center: [-73.5, -40.5],
-  rotation: rotation,
-  constraints:{rotationEnabled: false,}
+  scale : 80000000,
+  center: [12, 12],
+  constraints:{
+    rotationEnabled: false,
+    minScale: 90000000,
+    maxScale: 10000000,    
+  }
 })
-
-//view.when(() => console.log('Map and View are ready'));
-// Create a button
-const button = document.createElement('button');
-button.textContent = 'Take Snapshot';
-app.appendChild(button);
-
-// Add an event listener to the button
-button.addEventListener('click', () => {
-  // Take a screenshot of the view
-  view.takeScreenshot({ format: 'jpg' }).then((screenshot) => {
-    // Convert the data URL to a blob
-    const blob = dataURLToBlob(screenshot.dataUrl);
-
-    // Create a link and use it to download the blob as a JPEG file
-    const link = document.createElement('a');
-    link.href = URL.createObjectURL(blob);
-    link.download = 'map-snapshot.jpg';
-    link.click();
+let oid: number;
+view.on('click', function(event){
+  countryGraphicsLayer.graphics.removeAll();
+  console.log(event.mapPoint)
+  const opts = {
+    include: layer
+  }
+  view.hitTest(event, opts).then((response) => {
+    if (response.results.length) {
+      let pi = response.results[0]
+      if (pi.type==="graphic"){
+        let grafo = pi.graphic
+        if (countryGraphicsLayer.graphics.length==0){
+          grafo.symbol=zz
+          countryGraphicsLayer.graphics.add(grafo)
+          oid = pi.graphic.attributes["OBJECTID"]
+          console.log(oid)
+        } else if (grafo.attributes["OBJECTID"]!=oid){
+          countryGraphicsLayer.graphics.removeAll();
+          grafo.symbol=zz
+          oid = grafo.attributes["OBJECTID"]
+          console.log(oid)
+          countryGraphicsLayer.graphics.add(grafo);
+        }        
+      } 
+    } else {
+      countryGraphicsLayer.graphics.removeAll();
+    }
   });
 });
-
-// Function to convert a data URL to a blob
-function dataURLToBlob(dataUrl: any) {
-  const binary = atob(dataUrl.split(',')[1]);
-  let array = [];
-  for (let i = 0; i < binary.length; i++) {
-    array.push(binary.charCodeAt(i));
-  }
-  return new Blob([new Uint8Array(array)], { type: 'image/jpeg' });
-}
-// Watch the width breakpoint of the view and update the view accordingly
-watch(
-  () => view.widthBreakpoint,
-  (breakpoint) => {
-    console.log('Width breakpoint:', breakpoint);
-    switch (breakpoint) {
-      case 'xsmall':
-        view.ui.remove('zoom');
-        view.rotation = 0;
-        break;
-      default:
-        view.rotation = -90;
-  }
-  },
-);
